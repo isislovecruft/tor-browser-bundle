26e0cd44f2886bfad1c3d30844ff7a21eb9d0478
David Fifield <david@bamsoftware.com>
2017-07-05 22:57:22 -0700

    Build go-webrtc and snowflake in the mac pluggable-transports descriptor.
    I had to apply two tricks to get a reproducible snowflake-client.
    
    The first is to use faketime to eliminate some timestamps. There were 11
    variable timestamps in the file. Through experimentation, I found that
    10 of them were dependent on the Go runtime (recompiling Go caused them
    to change) and 1 was dependent on snowflake-client itself (recompiling
    snowflake-client with the same runtime changed only that 1 timestamp).
    The underlying issue has to do with clang 3.8.0 on Darwin embedding
    timestamps, unsolved in the Go issue tracker as of 13 days ago.
    https://github.com/golang/go/issues/9206#issuecomment-310476743
    
    The second is a sed command to clobber embedded paths of the form
    /tmp/go-buildXXXXXXXXX and /tmp/go-link-XXXXXXXXX. Their presence is
    caused by some combination of Clang and Darwin, and there is as yet no
    known workaround upstream.

--- gitian/mkbundle-mac.sh
+++ gitian/mkbundle-mac.sh
@@ -240,7 +240,7 @@ then
   echo "****** Starting Pluggable Transports Component of Mac Bundle (5/6 for Mac) ******"
   echo
 
-  ./bin/gbuild -j $NUM_PROCS -m $VM_MEMORY --commit goptlib=$GOPTLIB_TAG,meek=$MEEK_TAG,ed25519=$GOED25519_TAG,siphash=$GOSIPHASH_TAG,goxcrypto=$GO_X_CRYPTO_TAG,goxnet=$GO_X_NET_TAG,obfs4=$OBFS4_TAG $DESCRIPTOR_DIR/mac/gitian-pluggable-transports.yml
+  ./bin/gbuild -j $NUM_PROCS -m $VM_MEMORY --commit goptlib=$GOPTLIB_TAG,meek=$MEEK_TAG,ed25519=$GOED25519_TAG,siphash=$GOSIPHASH_TAG,goxcrypto=$GO_X_CRYPTO_TAG,goxnet=$GO_X_NET_TAG,obfs4=$OBFS4_TAG,go-webrtc=$GO_WEBRTC_TAG,snowflake=$SNOWFLAKE_TAG,uniuri=$UNIURI_TAG $DESCRIPTOR_DIR/mac/gitian-pluggable-transports.yml
   if [ $? -ne 0 ];
   then
     #mv var/build.log ./firefox-fail-mac.log.`date +%Y%m%d%H%M%S`
